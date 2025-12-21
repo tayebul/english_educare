b84e61a14705200e44a7642123ff07b92b5ae99b
Your Nametayebul <you@example.comabir.eee42.aust@gmail.com>
2025-12-21 22:37:07 +0600

    fixed small bug

--- script_main_page.js
+++ script_main_page.js
@@ -1,4 +1,4 @@
 document.getElementById("logout_btn").addEventListener("click" , (e) => {
     e.preventDefault();
-    window.location.href = "homepage.html";
+    window.location.href = "index.html";
 })
