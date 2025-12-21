1aefbaeaa473fc1942c22941bc6a1427fce36c22
Your Nametayebul <you@example.comabir.eee42.aust@gmail.com>
2025-12-21 22:26:32 +0600

    added main page

---
+++ script_homepage.js
@@ -0,0 +1,5 @@
+document.getElementById("login_btn").addEventListener("click" , (e) => {
+    e.preventDefault();
+    window.location.href = "main_page.html";
+})
+
---
+++ script_main_page.js
@@ -0,0 +1,4 @@
+document.getElementById("logout_btn").addEventListener("click" , (e) => {
+    e.preventDefault();
+    window.location.href = "homepage.html";
+})
