44ed688246156ed452a83d0a951f399142cea2aa
Your Nametayebul <you@example.comabir.eee42.aust@gmail.com>
2025-12-22 14:19:15 +0600

    api integration

--- script_main_page.js
+++ script_main_page.js
@@ -6,3 +6,24 @@ document.getElementById("logout_btn_responsive").addEventListener("click" , (e) 
     e.preventDefault();
     window.location.href = "index.html";
 })
+
+
+
+const load_lessons = () => {
+    fetch("https://openapi.programming-hero.com/api/levels/all")
+    .then(level_res => level_res.json()).then(level_data => display_lessons(level_data.data) )
+}
+
+const display_lessons = (lesson) => {
+    const levelContainer = document.getElementById("level_container");
+    levelContainer.innerHTML = " "
+   
+    for(let les of lesson) {
+        console.log(les)
+        const level_box = document.createElement("div");
+        level_box.innerHTML = ` <button class="btn btn-outline btn-primary"><i class="fa-solid fa-person-chalkboard"></i> Lesson - ${les.level_no} </button> ` ;
+
+        levelContainer.append(level_box);
+    }
+}
+load_lessons()
